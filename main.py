12e41aead307b0ca2c9d5ad821e827d1fd01255d
TheFoxPlush <leredstoneur98@gmail.com>
2026-08-17 19:39:50 +0200

    wrongful error in mineskin code error notification

--- main.py
+++ main.py
@@ -1096,7 +1096,7 @@ def get_head_id_from_tile(tile,name): #generates a head id from mineskin
                     Notification(root,"Your api key is likely incorrect. Shutting down task...","error")
                     return(None)
                 else:
-                    Notification(root,f"Error from mineskin.org (response.status_code). Trying again in 5s...")
+                    Notification(root,f"Error from mineskin.org ({response.status_code}). Trying again in 5s...")
                     sleep(5)
                     continue
             break
